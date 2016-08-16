9ddd8b0c4bd6c17e38db80cd1942f53155b80644
wink94 <mwindula16@gmail.com>
2016-08-17 02:23:14 +0530

    dbinsert

--- LabManagement/DBAccess/DBInsert.cs
+++ LabManagement/DBAccess/DBInsert.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Configuration;
 using System.Windows.Forms;
+using LabManagement.Controllers;
 
 namespace LabManagement.DBAccess
 {
@@ -14,14 +15,30 @@ namespace LabManagement.DBAccess
         MySqlConnection conn;
         public DBInsert()
         {
-            
                 conn = ConnectionManager.GetConnection();
-                conn.Open();
-                
-           
         }
-        /*insert patient details to DB*/
 
+        /*insert patient details to DB*/
+        public void insertLabPatient(LabPatient lp)
+        {
+            string query;
+
+            try
+            {
+                conn.Open();
+
+                query = "insert into lab_patient(labPatientName,labPatientAge,labPatientGender,labPatientEmail,labPatientPhone) values('"+lp.Name+"','"+lp.Age+"','"+lp.Gender+"','"+lp.Email+"','"+lp.Phone+"')";
+
+                MySqlCommand newCmd = new MySqlCommand(query, conn);
+                newCmd.ExecuteNonQuery();
+
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("DB Error :" + e.Message);
+            }
+            conn.Close();
+        }
 
     }
 }
